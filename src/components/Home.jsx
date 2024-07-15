8ec4a495d63cb3b45b46aecffe90c82e01a4d6e9
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-07-15 22:04:48 +0530

    will continue afterwards

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='home'>Home <ProductCard /></div>
+  )
+
+}
+const ProductCard = ({ name, id, price, handler, imgSrc }) => (
+  <div className="productCard">
+    <img src={imgSrc} alt={name} />
+    <p>{name}</p>
+    <h4>${price}</h4>
+    <button onClick={() => handler({ name, price, id, quantity: 1, imgSrc })}>
+      Add to Cart
+    </button>
+  </div>
+);
+
+export default Home
